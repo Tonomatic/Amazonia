3f46b495a1d71e006ddffdd9c275dda080bf2560
Tonomatic <antonysoly2217@gmail.com>
2021-08-14 22:11:28 -0400

    working on the dynamic data of Amazonia

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="rows">
+        <div>
+          <a className="brand" href="index.html">amazonia</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main>
+        <div className="rows center">
+          {data.products.map((item) => (
+            <div key={item._id} className="card">
+              <a href={`/product/${item._id}`}>
+                <img className="mediumImg" src={item.image} alt="product" />
+              </a>
+              <div className="card-body">
+                <a href={`/product/${item._id}`}>
+                  <h2>{item.name}</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">${item.price}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer className="rows center">GiHub LinkedIn</footer>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Gucci Shirt',
+            category: 'Shirts',
+            image: '/images/dude.jpg',
+            price: 200,
+            brand: 'Gucci',
+            rating: 5,
+            numReviews: 9,
+            description: 'stylish gucci shirt'
+        },
+        {
+            _id: '1',
+            name: 'Gucci Shirt',
+            category: 'Shirts',
+            image: '/images/dude.jpg',
+            price: 50,
+            brand: 'Gucci',
+            rating: 5,
+            numReviews: 9,
+            description: 'stylish gucci shirt'
+        },
+        {
+            _id: '1',
+            name: 'Gucci Shirt',
+            category: 'Shirts',
+            image: '/images/dude.jpg',
+            price: 300,
+            brand: 'Gucci',
+            rating: 5,
+            numReviews: 9,
+            description: 'stylish gucci shirt'
+        },
+        {
+            _id: '1',
+            name: 'Gucci Shirt',
+            category: 'Shirts',
+            image: '/images/dude.jpg',
+            price: 100,
+            brand: 'Gucci',
+            rating: 5,
+            numReviews: 9,
+            description: 'stylish gucci shirt'
+        },
+        {
+            _id: '1',
+            name: 'Gucci Shirt',
+            category: 'Shirts',
+            image: '/images/dude.jpg',
+            price: 200,
+            brand: 'Gucci',
+            rating: 5,
+            numReviews: 9,
+            description: 'stylish gucci shirt'
+        },
+        {
+            _id: '1',
+            name: 'Gucci Shirt',
+            category: 'Shirts',
+            image: '/images/dude.jpg',
+            price: 200,
+            brand: 'Gucci',
+            rating: 3.5,
+            numReviews: 2,
+            description: 'stylish gucci shirt'
+        }
+    ]
+}
+
+
+export default data;
