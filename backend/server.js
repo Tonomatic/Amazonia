3e57bb1941d5189c98c58ec1b7778739914841cf
Tonomatic <antonysoly2217@gmail.com>
2021-09-08 21:23:19 -0400

    added redux functionalityty to the individual products

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,18 @@ app.get('/api/products', (req, res) => {
     res.send(data.products);
 })
 
+
+//:id is a placeholder for id of product
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find( x => x._id === req.params.id);
+    if(product) {
+        res.send(product);
+    } else {
+        res.status(404).send({message: "Product not Found"});
+    }
+})
+
+
 app.get('/', (req, res) => {
     res.send('Sever is ready')
 });
